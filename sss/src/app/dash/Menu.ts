233bcdce95a149a75fa7636fdc90618f970f406f
IT-DUY\IT DUY <itcenter@thaituan.com.vn>
2023-05-10 13:27:40 +0700

    phuc pussh

---
+++ sss/src/app/dash/Menu.ts
@@ -0,0 +1,9 @@
+export class Menu {
+    constructor(
+        public  Menu_Name:string ,
+        public  ParentId:number  ,
+        public  PageId:number ,
+        public  oder_no:number ,
+        public  Icon:string ,
+    ) {}
+}
